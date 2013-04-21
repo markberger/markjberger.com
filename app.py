b1223ea051c3ef57f25d49571e8bbc153e66fc09
Mark Berger <mark.berger.j@gmail.com>
2013-04-20 17:04:13 -0700

    Slight change to app.py and fixed bottom navbar on smaller devices

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
+def main():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    main()
