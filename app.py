e20d63dbc9cbc7e66a98fdd741ddee6ac1fd295e
Mark Berger <mark.berger.j@gmail.com>
2013-06-22 14:20:12 -0400

    Adds a projects page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
 def main():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
